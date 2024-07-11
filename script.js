282bec3da99581608f05372da6148ca8927b20c5
MasomRezaie <masom.rezaie135@gmail.com>
2024-07-10 22:17:12 -0700

    added functions for price and number

--- script.js
+++ script.js
@@ -21,3 +21,38 @@ document.getElementById('price').innerHTML = `<strong>Price:</strong> ${price.to
 const displayCashInDrawer = () => {
   displayCid.innerHTML = `<h4>Cash in Drawer:</h4>${cid.map(([denomination, amount]) => `${denomination}: $${amount.toFixed(2)} <br>`).reverse().join('')}`;
 };
+
+const calculateChange = (cash) => {
+  const change = Number((cash - price).toFixed(2));
+  const totalCid = Number(cid.reduce((total, [, amount]) => total + amount, 0).toFixed(2));
+
+  if (change > totalCid) {
+    return { status: 'INSUFFICIENT_FUNDS', change: 0 };
+  }
+
+  const denominations = [100, 20, 10, 5, 1, 0.25, 0.1, 0.05, 0.01];
+  const denominationNames = ['ONE HUNDRED', 'TWENTY', 'TEN', 'FIVE', 'ONE', 'QUARTER', 'DIME', 'NICKEL', 'PENNY'];
+  const changeArr = [];
+  const cidCopy = [...cid];
+
+  for (let i = 0; i < denominations.length; i += 1) {
+    let totalDenom = 0;
+    while (change >= denominations[i] && cidCopy[cidCopy.length - 1 - i][1] > 0) {
+      cidCopy[cidCopy.length - 1 - i][1] = Number((cidCopy[cidCopy.length - 1 - i][1] - denominations[i]).toFixed(2));
+      change = Number((change - denominations[i]).toFixed(2));
+      totalDenom += denominations[i];
+    }
+    if (totalDenom > 0) {
+      changeArr.push([denominationNames[i], totalDenom]);
+    }
+  }
+
+  if (change > 0) {
+    return { status: 'INSUFFICIENT_FUNDS', change: 0 };
+  }
+
+  const remainingCid = cidCopy.reduce((total, [, amount]) => total + amount, 0);
+  const status = remainingCid === 0 ? 'CLOSED' : 'OPEN';
+
+  return { status, change: changeArr };
+};
