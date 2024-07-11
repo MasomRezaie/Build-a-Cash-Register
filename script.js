51bbfb9b6f35cf82f77024edbc7b53f40f7f046a
MasomRezaie <masom.rezaie135@gmail.com>
2024-07-10 22:10:00 -0700

    const variable added

---
+++ script.js
@@ -0,0 +1,17 @@
+const cashInput = document.getElementById('cash');
+const displayChangeDue = document.getElementById('change-due');
+const purchaseBtn = document.getElementById('purchase-btn');
+const displayCid = document.getElementById('cashdrawer');
+const price = 19.5;
+
+const cid = [
+  ['PENNY', 0.01],
+  ['NICKEL', 0],
+  ['DIME', 0],
+  ['QUARTER', 0],
+  ['ONE', 0],
+  ['FIVE', 0],
+  ['TEN', 0],
+  ['TWENTY', 0],
+  ['ONE HUNDRED', 0],
+];
