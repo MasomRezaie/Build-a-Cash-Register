d32912f85a5dedccbdd696c60213835ab339d439
MasomRezaie <masom.rezaie135@gmail.com>
2024-07-10 22:11:50 -0700

    an element linked bt id from html to script

--- script.js
+++ script.js
@@ -15,3 +15,9 @@ const cid = [
   ['TWENTY', 0],
   ['ONE HUNDRED', 0],
 ];
+
+document.getElementById('price').innerHTML = `<strong>Price:</strong> ${price.toFixed(2)}`;
+
+const displayCashInDrawer = () => {
+  displayCid.innerHTML = `<h4>Cash in Drawer:</h4>${cid.map(([denomination, amount]) => `${denomination}: $${amount.toFixed(2)} <br>`).reverse().join('')}`;
+};
