e8ec775f9a12f8e465d63003db64535c2c6a0e7f
MasomRezaie <masom.rezaie135@gmail.com>
2024-07-10 22:55:04 -0700

    chabged the numbers

--- script.js
+++ script.js
@@ -5,15 +5,15 @@ const displayCid = document.getElementById('cashdrawer');
 const price = 19.5;
 
 const cid = [
-  ['PENNY', 0.01],
-  ['NICKEL', 0],
-  ['DIME', 0],
-  ['QUARTER', 0],
-  ['ONE', 0],
-  ['FIVE', 0],
-  ['TEN', 0],
-  ['TWENTY', 0],
-  ['ONE HUNDRED', 0],
+  ['PENNY', 1.01],
+  ['NICKEL', 2.05],
+  ['DIME', 3.1],
+  ['QUARTER', 4.25],
+  ['ONE', 90],
+  ['FIVE', 55],
+  ['TEN', 20],
+  ['TWENTY', 60],
+  ['ONE HUNDRED', 100],
 ];
 
 document.getElementById('price').innerHTML = `<strong>Price:</strong> ${price.toFixed(2)}`;
